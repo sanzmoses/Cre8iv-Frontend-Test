1523fed1735b0efb1378728f2f861d7a9d5ebcac
sanzmoses <sanzmoses@gmail.com>
2019-08-23 15:57:22 +0800

    rearrange

---
+++ assets/js/_base.js
@@ -0,0 +1,8 @@
+//@codekit-prepend '../node_modules/uikit/dist/js/uikit.js'
+//@codekit-prepend '../node_modules/uikit/dist/js/uikit-icons.js'
+
+var answer = 42;
+
+function sayHello(){
+    console.log('hello');
+}
